ec92e33d2701929bdecfbb00b1b3ff7ee20c5cd1
Nikita Golik <nikitq4a@gmail.com>
2021-03-01 14:45:11 +0300

    Dependency injection:property, value,setter, list

---
+++ springappreply/src/main/java/by/golik/spring/DiscoMusic.java
@@ -0,0 +1,11 @@
+package by.golik.spring;
+
+/**
+ * @author Nikita Golik
+ */
+public class DiscoMusic implements Music {
+    @Override
+    public String getSong() {
+        return "This is San-Francisko";
+    }
+}
--- springappreply/src/main/java/by/golik/spring/MusicPlayer.java
+++ springappreply/src/main/java/by/golik/spring/MusicPlayer.java
@@ -1,17 +1,42 @@
 package by.golik.spring;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Nikita Golik
  */
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
+    private String name;
+    private int volume;
 
-    //IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public MusicPlayer() {}
+
+
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
