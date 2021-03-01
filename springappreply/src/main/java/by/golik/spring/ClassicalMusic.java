edd0c9f0c10c578577e717dc5670aad33b4b67c9
Nikita Golik <nikitq4a@gmail.com>
2021-03-01 15:40:03 +0300

    lifecycle,factory method

--- springappreply/src/main/java/by/golik/spring/ClassicalMusic.java
+++ springappreply/src/main/java/by/golik/spring/ClassicalMusic.java
@@ -4,6 +4,16 @@ package by.golik.spring;
  * @author Nikita Golik
  */
 public class ClassicalMusic implements Music {
+    private ClassicalMusic() {}
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+    public void doMyDestroy() {
+        System.out.println("Doing my destroying");
+    }
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
