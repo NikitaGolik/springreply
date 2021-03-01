12c50f7ed66a9797f1620441d68bfb7a9b785f6c
Nikita Golik <nikitq4a@gmail.com>
2021-03-01 15:13:12 +0300

    scope prototype

--- springappreply/src/main/java/by/golik/spring/MusicPlayer.java
+++ springappreply/src/main/java/by/golik/spring/MusicPlayer.java
@@ -7,13 +7,10 @@ import java.util.List;
  * @author Nikita Golik
  */
 public class MusicPlayer {
-    private List<Music> musicList = new ArrayList<>();
+    private Music music;
     private String name;
     private int volume;
 
-    public void setMusicList(List<Music> musicList) {
-        this.musicList = musicList;
-    }
 
     public String getName() {
         return name;
@@ -31,12 +28,18 @@ public class MusicPlayer {
         this.volume = volume;
     }
 
+    //IoC
+    public MusicPlayer(Music music) {
+        this.music = music;
+    }
     public MusicPlayer() {}
 
+    public void setMusic(Music music) {
+        this.music = music;
+    }
 
     public void playMusic() {
-        for (Music music : musicList) {
-            System.out.println("Playing: " + music.getSong());
-        }
+        System.out.println("Playing: " + music.getSong());
     }
+
 }
