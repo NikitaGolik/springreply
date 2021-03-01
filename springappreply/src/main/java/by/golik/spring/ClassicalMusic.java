2a4dc5ccfb7de51a3ca90020315eda6b07e463f7
Nikita Golik <nikitq4a@gmail.com>
2021-03-01 16:52:48 +0300

    predestroy, postconstruct, scope

--- springappreply/src/main/java/by/golik/spring/ClassicalMusic.java
+++ springappreply/src/main/java/by/golik/spring/ClassicalMusic.java
@@ -1,6 +1,10 @@
 package by.golik.spring;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 /**
  * @author Nikita Golik
@@ -8,6 +12,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class ClassicalMusic implements Music {
 
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("Doing my destroying");
+    }
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
